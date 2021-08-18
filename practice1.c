0870d53cf12f07b14cc4e6062ed86bb412c8f2de
LCabrerag <85259439+LCabrerag@users.noreply.github.com>
2021-08-17 22:30:17 -0500

    Update practice1.c

--- practice1.c
+++ practice1.c
@@ -1,47 +1,61 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     int x1 = 2;
     int y1 = 6;
 
     int x2 = 12;
-    int y2 = 2;
+    int y2 = 6;
 
     double xtmp = x1;
     double ytmp = y1;
     
     int xend = x1;
     int yend = y1;
+    
+    double m = 0;
 
-    // formula de la recta y - y1 = m ( x - x1 )
-    double m = ((double)y2 - (double)y1)/((double)x2 - (double)x1);
+    
+    
     
     printf("X Y\n");
     printf("%d %d\n", x1, y1);
     
-    while ((int)xend != x2 && (int)yend != y2){
-        if (x1 < x2){
-            xtmp += .1;
-            ytmp = m*(xtmp - (double)x1) + (double)y1;
-        }
-        if (x1 == x2){
-            ytmp++;
-        }
-        if (x1 > x2){
-            xtmp -= .1;
-            ytmp = m*(xtmp - (double)x1) + (double)y1;
-            
-        }
-        printf("%d %d\n", (int)xtmp, (int)ytmp);
+    if(y2 != y1){
+        // Slope m  = (y2 - y1)/(x2 - x1) 
+        m = ((double)y2 - (double)y1)/((double)x2 - (double)x1);
+        while (xend != x2 || yend != y2){
+            if (x1 < x2){
+                xtmp += .1;
+                // formula de la recta y = m ( x - x1 ) + y1
+                ytmp = m*(xtmp - (double)x1) + (double)y1;
+            }
+            if (m == 0){
+                ytmp++;
+            }
+            if (x1 > x2){
+                xtmp -= .1;
+                // formula de la recta y = m ( x - x1 ) + y1
+                ytmp = m*(xtmp - (double)x1) + (double)y1;
+                
+            }
+            if ( round(xtmp) != xend || round(ytmp) != yend){
+                xend = round(xtmp);
+                yend = round(ytmp);
+                printf("%d %d\n", xend, yend);
+            }
         
-        if ((int)xtmp != xend && (int)ytmp != yend){
-            xend = (int)xtmp;
-            yend = (int)ytmp;
+        }
+    }else{
+        while (xend != x2){
+            xend += 1.0;
             printf("%d %d\n", xend, yend);
         }
-    
+        
     }
     
 
     return 0;
 }
+    
